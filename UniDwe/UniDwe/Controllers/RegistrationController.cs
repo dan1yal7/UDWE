13b0095de5e8020224d440847be592b4833fad33
Daniyal <daniiltemirkhanov@gmail.com>
2025-03-26 23:55:45 +0500

    RegistrationController prerequsites

--- UniDwe/UniDwe/Controllers/RegistrationController.cs
+++ UniDwe/UniDwe/Controllers/RegistrationController.cs
@@ -1,12 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Internal;
+using UniDwe.Models.ViewModel;
+using UniDwe.Services;
 
 namespace UniDwe.Controllers
 {
+    [Controller]
     public class RegistrationController : Controller
     {
-        public IActionResult Index()
+        private readonly IRegistrationSerivce _registrationService;
+        public RegistrationController(IRegistrationSerivce registrationSerivce) 
         {
-            return View();
+            _registrationService = registrationSerivce;
+        }
+
+        [HttpGet]
+        [Route("/register")]
+        public IActionResult Index()    
+        {
+            return View("Index", new RegistrationViewModel());
         }
     }
 }
