c53dda6feac89e6ed7b8b2d469f3836e9ad5fa1d
Daniyal <daniiltemirkhanov@gmail.com>
2025-04-05 17:27:03 +0500

    Client-side validation attributes

--- UniDwe/UniDwe/Models/ViewModel/LoginViewModel.cs
+++ UniDwe/UniDwe/Models/ViewModel/LoginViewModel.cs
@@ -5,6 +5,7 @@ namespace UniDwe.Models.ViewModel
     public class LoginViewModel
     {
         [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
         public string? Email { get; set; }
 
         [Required(ErrorMessage = "Password is required")]
