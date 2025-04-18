70277b4667c8f2c61ca9091124dc2ccb199e74a4
Daniyal <daniiltemirkhanov@gmail.com>
2025-04-19 00:09:54 +0500

    DbSession model

---
+++ UniDwe/UniDwe/Session/DbSession.cs
@@ -0,0 +1,11 @@
+﻿namespace UniDwe.Session
+{
+    public class DbSession
+    {
+        public Guid SessionId { get; set; }
+        public string? SessionData { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastAccessed { get; set; }
+        public int? UserId { get; set; }
+    }
+}
