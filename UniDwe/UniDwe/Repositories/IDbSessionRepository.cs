25678b3e1a41ed6636776a751070fb43c027b967
Daniyal <daniiltemirkhanov@gmail.com>
2025-04-21 00:07:39 +0500

    DbSessionRepository for CRUD operations and IDbSessionService

---
+++ UniDwe/UniDwe/Repositories/IDbSessionRepository.cs
@@ -0,0 +1,37 @@
+﻿using UniDwe.Infrastructure;
+using UniDwe.Session;
+
+namespace UniDwe.Repositories
+{
+    public interface IDbSessionRepository
+    {
+        Task<DbSession> GetSessionAsync(Guid sessionId);
+        Task<int> UpdateSession(DbSession session);
+        Task<int> CreateSession(Guid sessionId);
+    }
+
+    public class DbSessionRepository : IDbSessionRepository
+    { 
+        private readonly ApplicationDbContext _dbContext;
+
+        public DbSessionRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Task<int> CreateSession(Guid sessionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DbSession> GetSessionAsync(Guid sessionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> UpdateSession(DbSession session)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
